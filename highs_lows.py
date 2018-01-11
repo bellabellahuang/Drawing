5abf20aa823f33f3f436e56aa9567886afe9276d
bellahuang <bellahuang1012@gmail.com>
2018-01-10 19:54:01 -0500

    learning pyplot

---
+++ highs_lows.py
@@ -0,0 +1,38 @@
+import csv
+
+from matplotlib import pyplot as plt 
+from datetime import datetime
+
+filename = 'death_valley_2014.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	highs = []
+	dates = []
+	lows = []
+	for row in reader:
+		try:
+			high = int(row[1])
+			low = int(row[3])
+			date = datetime.strptime(row[0], "%Y-%m-%d")
+		except ValueError:
+			print(date, 'missing data')
+		else:
+			highs.append(high)
+			dates.append(date)
+			lows.append(low)
+
+	fig = plt.figure(dpi=128, figsize=(10, 6))
+	plt.plot(dates, highs, c='red', alpha=0.5)
+	plt.plot(dates, lows, c='blue', alpha=0.5)
+	plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+	plt.title("Daily high and low temperatures - 2014", fontsize=24)
+	plt.xlabel('', fontsize=16)
+	fig.autofmt_xdate()
+	plt.ylabel('Temperature (F)', fontsize=16)
+	plt.tick_params(axis='both', which='major', labelsize=16)
+
+	plt.show()
+
