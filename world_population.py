332a378ab1b668215f86664c915afcac4bb1c46b
bellahuang <bellahuang1012@gmail.com>
2018-01-13 20:12:28 -0500

    modified world_population.py to show population numbers

--- world_population.py
+++ world_population.py
@@ -1,17 +1,22 @@
 import json
-
+from pygal.maps.world import World 
 from country_codes import get_country_code
 
 filename = 'population_data.json'
 with open(filename) as f:
 	p_data = json.load(f)
 
+cc_populations = {} # new dictionary
 for p in p_data:
 	if p['Year'] == '2010':
 		country_name = p['Country Name']
 		population = int(float(p['Value']))
 		code = get_country_code(country_name)
 		if code:
-			print(code + ": " + str(population))
-		else:
-			print('ERROR - ' + country_name)
+			cc_populations[code] = population 
+
+wm = World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
